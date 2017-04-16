5f76ad24f1de2ef35b58772b01933c61a03808a3
Yuichi Senga <subent@noonz.net>
2017-04-16 21:12:25 +0900

    ダミー追加

---
+++ src/local/en.js
@@ -0,0 +1,90 @@
+{
+  "title": "page title",
+  "description": "DESCRIPTION",
+  "keyword": "keyword",
+  "author": "author",
+  "nav": {
+    "home": "nav.Home",
+    "overview": "nav.Overview",
+    "service": "nav.Service",
+    "portfolio": "nav.Portfolio",
+    "about_us": "nav.About Us",
+    "contact_us": "nav.Contact Us",
+    "current_language": "EN"
+  },
+  "home": {
+    "title": "home.title",
+    "catch1": "home.catch1",
+    "catch2": "home.catch2",
+    "catch3": "home.catch3"
+  },
+  "meet_us": {
+    "title": "meat-us.title",
+    "caption": "meat-us.content"
+  },
+  "service": {
+    "title": "service.title",
+    "caption": "service.caption",
+    "item1_title": "service.titleA",
+    "item1_caption": "service.captionA",
+    "item2_title": "service.titleB",
+    "item2_caption": "service.captionB",
+    "item3_title": "service.titleC",
+    "item3_caption": "service.captionC"
+  },
+  "works": {
+    "title": "works.title",
+    "caption": "works.caption",
+    "item1_title": "works.titleA",
+    "item1_caption": "works.captionA",
+    "item2_title": "works.titleB",
+    "item2_caption": "works.captionB",
+    "item3_title": "works.titleC",
+    "item3_caption": "works.captionC",
+    "item4_title": "works.titleD",
+    "item4_caption": "works.captionD",
+  },
+  "partner": {
+    "title": "partner.title",
+    "caption": "partner.caption"
+  },
+  "about_us": {
+    "title": "about_us.title",
+    "caption": "about_us.caption"
+  },
+  "testimonial": {
+    "title": "testimonial.title",
+    "content": [
+      {
+        "caption": "testimonial.caption1",
+        "author": "testimonial.author1"
+      },
+      {
+        "caption": "testimonial.caption2",
+        "author": "testimonial.author2"
+      },
+      {
+        "caption": "testimonial.caption3",
+        "author": "testimonial.author3"
+      },
+      {
+        "caption": "testimonial.caption4",
+        "author": "testimonial.author4"
+      }
+    ]
+  },
+  "contact": {
+    "title": "contact.title",
+    "address_title": "address",
+    "address": "AAA<br>BBB<br>CCC<br>Phone",
+    "your_name": "contact.your_name",
+    "email": "contact.Email",
+    "subject": "contact.subject",
+    "message": "contact.message",
+    "send": "Scontact.send"
+  },
+  "footer": {
+    "copyright": "footer.copyright",
+    "back_top": "footer.back_top"
+  }
+}
